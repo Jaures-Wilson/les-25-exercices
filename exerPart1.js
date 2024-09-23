d71df7df3b729e356ac9b864c19c523bd3123127
Jaurès Wilson <mboumelaj@gmail.com>
2024-09-23 15:12:02 +0200

    les 25 exercices de Javascripts

--- exerPart1.js
+++ exerPart1.js
@@ -1,91 +1,93 @@
 // EXERCICE 1
-/* let euler = 2.71828
-console.log(euler.toFixed(4)) 
- */
-
+function truncateANumberTo4DigitsAfterDecimalPoint(){ 
+    let euler = 2.71828
+    return euler.toFixed(4);
+}
 
 
 //EXERCICE 2
-/* const findMax = (arg) => {
+const findMax = (arg) => {
      return Math.max(...arg)
  }
-console.log(findMax([1,2,3,6,5,4,9,8,7])); */ 
 
 
 
 //EXERCICE 3
-/* function display(quantity,product,price) {
+function display(quantity,product,price) {
     return `You purchased ${quantity} units of ${product} for ${price}`;
 }
-console.log(display(3,'biscuit',500))  */
+
  
-
-
 
 //EXERCICE 4
-/* function combinedArr(array1,array2) {
-        return [...array1,...array2].sort((a,b)=>a-b) 
+function combinedArr(array1,array2) {
+        return [...new Set(array1.concat(array2))].sort((a,b)=>a-b) 
     }
-console.log(combinedArr([984,54,684,6841,35,84],[651,98,65,684,5.1,91,3541,651,99])) */
- 
+//console.log(combinedArr([1,2,3],[1,2,3,4,5]))
 
 
 // EXERCICE 5
-/* const car = {
-    make : 'Cameroun',
-    model : 'toyota',
-    year : 'unknown'
+function destructionOnCar(){
+    const car = {make : 'Cameroun',model : 'toyota',year : '2012'}
+    const {make : val1, model : val2, year = 'unknown'} = car
+    return [val1, val2, year]
 }
-function destructionOnCar(obj){
-    const {make : val1, model : val2} = obj
-    return [val1, val2]
-}
-console.log(destructionOnCar(car))    */
-
+console.log(destructionOnCar())
 
 
 // EXERCICE 6 
-/* const calculateArea = (radius) => {
+const calculateArea = (radius) => {
      return radius !== undefined ? radius * radius * Math.PI : "radius is missing"
 }
-console.log(calculateArea(4))   */
+  
 
 
 
 // EXERCICE 7
-/* function average(...arg) {
+function average(...arg) {
     let amount = arg.reduce((accumulator, currentValue) => accumulator += currentValue ,0)
     return Math.round(amount/arg.length*100)/100;
 }
-console.log(average(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,99,-150));  */
+
   
 
 
 //EXERCICE 8
-/* function newArray(array) { 
+function newArray(array) { 
     return array.map(str => str.split('').reverse().join('')) 
-}
-
-let example = ["koki", "couscous", "banane malaxée", "eren yagers", ""];
-console.log(newArray(example))   */ 
+}  
 
 
 
 // EXERCICE 9
-/* library.filter(book => book.pages > 300)   */
+function displayBooksWithMoreThan300Pages(arrayObject){
+    return arrayObject.filter(book => book.pages > 300)
+}
+
 
 
 
 //EXERCICE 10
-/*  function fetchData() {
+function fetchData() {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve('Data fetched successfully!')
         },Math.floor(Math.random()*6000)+2000)
     })
 } 
-fetchData()
-    .then((item) => {
-        console.log(item)
-    }) 
- */
+
+
+
+
+module.exports = {
+    truncateANumberTo4DigitsAfterDecimalPoint,
+    findMax,
+    display,
+    combinedArr,
+    destructionOnCar,
+    calculateArea,
+    average,
+    newArray,
+    displayBooksWithMoreThan300Pages,
+    fetchData
+}  
--- exerPart2.js
+++ exerPart2.js
@@ -1,131 +1,100 @@
 // EXERCICE 1
-/* function FindTheRightNumber(inputNumber){
-    return Math.round(Math.random()*9)+1 === inputNumber ? 'Good Work' : 'Not matched'
-}
-
-console.log(FindTheRightNumber(5))  */
+function findTheRightNumber(inputNumber){
+    return Math.round(Math.random()*9)+1 === inputNumber ? 'Good Work' : 'Not matched';
+} 
 
 
 // EXERCICE 2
-/* function daysLeftUntilNextChrisams(){
+function daysLeftUntilNextChristmas(){
     let christmas = new Date((new Date).getFullYear(),11,25)
     if(christmas < (new Date)){
-        christmas = new Date(current.getFullYear()+1,11,25)
+        christmas = new Date((new Date).getFullYear()+1,11,25)
     }  
     return Math.floor((christmas - (new Date)) / (1000*60*60*24))  
 }
 
-console.log(daysLeftUntilNextChrisams())  */
    
 
-
-
 // EXERCICE 3
-/* function productAndMultiplicationOfTwoNumber(value1,value2){
-    if(value1 == 0 && value2 == 0){
-        return 'la division de la première valeur par la deuxième est indéterminée et le produit des deux valeurs est : ' + value1*value2
-    }else{ 
-        return 'le produit des deux valeurs est : '+ value1*value2 +' et la division de la première valeur par la deuxième est : '+ value1/value2
-    }  
+function productAndDivisionionOfTwoNumber(value1,value2){
+    return `the product is: ${value1*value2} and division is: ${value1/value2}`  
 }
-
-console.log(productAndMultiplicationOfTwoNumber(2,3))  */
 
 
 
 // EXERCICE 4
-/* function getLongestString(array) {
+function getLongestString(array) {
     let longerString = array.reduce((longer, current) => {
         return longer.length >= current.length ? longer : current
     }, 0)
     return longerString;
 }
-let exampleArray = ["koki", "couscous", "banane malaxée", "eren yagers", "", 5, 984];
-console.log('la chaine la plus longue du tableau est : ', getLongestString(exampleArray))  */ 
 
 
 
 // EXERCICE 5
-/* function highNumber(array){
+function highNumber(array){
     const newArray = array.filter(a => a % 2 === 0).reduce((accumulator,currentValue) => currentValue > accumulator ? accumulator = currentValue : accumulator = accumulator )
     return newArray
 }
 
-const integerExample = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 983, 2, -1000, -9, 0];
-console.log(highNumber(integerExample))  */
-
-
 
 
 // EXERCICE 6
-/* function characterDeleteRepetitive(str){
+function characterDeleteRepetitive(str){
     let arrStr = str.split('');
      const uniqueStr = [...new Set(arrStr)]
-
      return uniqueStr.join('');
 }
 
-let exampleString = "j'essaie d'écrire une chaîne choisie au hasard juste pour&é(§è!çà) essayer"
-console.log(characterDeleteRepetitive(exampleString))  */ 
-
+  
 
 
 // EXERCICE 7
-/* function sumCubedNumbers(number){
+function sumCubedNumbers(number){
     if(number === 0){
         return 0;
     }
     return Math.pow(number,3) + sumCubedNumbers(number - 1);
-}
-console.log(sumCubedNumbers(3))   */
-
+} 
 
 
 // EXERCICE 8
-/* function similarityCheck(obj1,obj2){
-    return Object.keys(obj2).every(a => obj1.hasOwnProperty(a) && obj1[a] === obj2[a]) ? "the first one contains equivalent property values to the second one" : "the first one doesn't contains equivalent property values to the second one"
+function similarityCheck(obj1,obj2){
+    return Object.keys(obj2).every(a => obj1.hasOwnProperty(a) && obj1[a] === obj2[a]) ? "equivalent property" : "different property"
 }
 
-const example1 = { prop1 : 'val1', prop2 : 'val2', prop3 : 'val3', prop4 : 'val4', prop5 : 'val5', prop6 : 'val6', prop7 : 'val7', prop8 : 'val8', prop9 : 'val9', prop10 : 'val10'}
-const example2 = { prop5 : 'val5', prop6 : 'val6', prop7 : 'val7', prop8 : 'val8', prop9 : 'val9', prop10 : 'val10'}
-console.log(similarityCheck(example1,example2)) */
 
 
 
 // EXERCICE 9
-/* function filterArray(arr, valuesToRemove) {
-    //valuesToRemove
-    return arr.filter(element => !valuesToRemove.includes(element));
-}
- */
+function filterArray(arr, valuesToRemove) {
+    return arr.filter(element => valuesToRemove !== element);
+} 
+
 
 
 
 //EXERCICE 10
-/* function extraction(array,...indexArray){
+function extraction(array,...indexArray){
     return indexArray.reduce((accumulator,currently) => accumulator.concat(array[currently]),[])
-}
-console.log(extraction([1,2,3,4,5,6],1,2,5))  */
+} 
 
+ 
 
 
 // EXERCICE 11
-/* function deletePropertyOfArray(object, property) {
+function deletePropertyOfArray(object, property) {
     let objectBefore = { ...object };  
     delete object[property];           
     return { before: objectBefore, after: object };  
 }
-var student = {
-    name: "David Rayy",
-    sclass: "VI",
-    rollno: 12
-}; 
-console.log(deletePropertyOfArray(student, "rollno"));   */
+  
 
 
 
 // EXERCICE 12
-/* class Cylinder {
+class Cylinder {
     constructor(height, radius){
         this.height = height,
         this.radius = radius
@@ -135,34 +104,38 @@ console.log(deletePropertyOfArray(student, "rollno"));   */
         return result.toFixed(4)
     }
 }
-let cylinderExample = new Cylinder(5.989819231,94.13516)
-console.log(cylinderExample.volume())   */
+
+function calculateVolumeCylinder(height,radius){
+    return (new Cylinder(height,radius)).volume()
+} 
+
 
 
 
 // EXERCICE 13
-/* function clock(second){
-    if(second > 0){
-        console.log((new Date()).getHours(),':',(new Date()).getMinutes(),':',(new Date()).getSeconds());
-        setTimeout(() => {clock(second-1)},1000);
+function clock(second) {
+    if (second > 0) {
+        console.log((new Date()).getHours(), ':', (new Date()).getMinutes(), ':', (new Date()).getSeconds());
+        timerId = setTimeout(() => { clock(second - 1); }, 1000);
+    } else {
+        clearTimeout(timerId); 
     }
 }
-clock(10)    */ 
+   
 
 
 
 // EXERCICE 14
-/* function checkIfStringIsLowerCaseOrUpperCase(string){
-    return string === string.toLowerCase() ? "la chaine est écrite en minuscule" : "la chaine n'est pas écrite en minuscule"
-}
+function checkIfStringIsLowerCaseOrUpperCase(string){
+    return string === string.toLowerCase() ? "lowercase" : "Not lowercase"
+} 
 
-console.log(checkIfStringIsLowerCaseOrUpperCase("andkajNdza"))  */
-
+ 
 
 
 
 // EXERCICE 15
-/* function add(num1,num2){
+function add(num1,num2){
     return new Promise((resolve,reject) => {
         if(arguments.length === 2){
             resolve(num1+num2)
@@ -171,12 +144,23 @@ console.log(checkIfStringIsLowerCaseOrUpperCase("andkajNdza"))  */
         }
     })
 }
-add(1,5)
-    .then((result) => {
-        console.log(result)
-    })
-    .catch((error)=>{
-        console.log(error)
-    })    */
+  
 
-        
+      
+module.exports = {
+    findTheRightNumber,
+    daysLeftUntilNextChristmas,
+    productAndDivisionionOfTwoNumber,
+    getLongestString,
+    highNumber,
+    characterDeleteRepetitive,
+    sumCubedNumbers,
+    similarityCheck,
+    filterArray,
+    extraction,
+    deletePropertyOfArray,
+    calculateVolumeCylinder,
+    checkIfStringIsLowerCaseOrUpperCase,
+    add,
+    clock 
+}
